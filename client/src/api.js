a42ace7329dc065b7b2e4fd0947830d5ad11817a
_Aditya_2603 <adithyarbi@gmail.com>
2025-11-20 16:09:20 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,4 @@
-export const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:4000';
+export const API_BASE = 'https://instagram-mlpe.onrender.com';
 
 export async function fetchPosts() {
   const res = await fetch(`${API_BASE}/api/posts`);
